093fecdb4c5a3a65c7a497fce6b0590eb3bf3861
yis <insuy672@gmail.com>
2025-03-06 17:35:15 +0900

    프로필 전체 레이아웃 수정

--- js/custom.js
+++ js/custom.js
@@ -68,32 +68,28 @@
 
 		//section위에서 마우스 휠을 움직이면
 		$("section").on("mousewheel",function(event,delta){    
-			  var ind = $(this).index();
-				var cnt = $("section").size()-1;
-		   console.log(cnt);
+			var ind = $(this).index();
+			var cnt = $("section").size()-1;
+		    console.log(cnt);
 			      //console.log(delta);
 						//위로휠(+1) , 아래로휠(-1)
             //마우스 휠을 올렸을때	
-              if (delta > 0 && ind!=0) { //+1 
-                //변수 prev에 현재 휠을 움직인 section에서 이전 section의 offset().top위치 저장
-                 var prev = $(this).prev().offset().top;
-                 //문서 전체를 prev에 저장된 위치로 이동
-                 $("html,body").stop().animate({"scrollTop":prev},'slow');
-                 return false;
-                //마우스 휠을 내렸을때	 
-              }else if (delta < 0 && ind!=cnt) {  //-1
-                //변수 next에 현재 휠을 움직인 section에서 다음 section의 offset().top위치 저장
-                 var next = $(this).next().offset().top;
-                 //문서 전체를 next에 저장된 위치로 이동
-                 $("html,body").stop().animate({"scrollTop":next},'slow');   
-                 return false;                                      
-              }
+			if (delta > 0 && ind!=0) { //+1 
+			//변수 prev에 현재 휠을 움직인 section에서 이전 section의 offset().top위치 저장
+				var prev = $(this).prev().offset().top;
+				//문서 전체를 prev에 저장된 위치로 이동
+				$("html,body").stop().animate({"scrollTop":prev},'slow');
+				return false;
+			//마우스 휠을 내렸을때	 
+			}else if (delta < 0 && ind!=cnt) {  //-1
+			//변수 next에 현재 휠을 움직인 section에서 다음 section의 offset().top위치 저장
+				var next = $(this).next().offset().top;
+				//문서 전체를 next에 저장된 위치로 이동
+				$("html,body").stop().animate({"scrollTop":next},'slow');   
+				return false;                                      
+			}
 			 
     });
-    
-  
-
-
 });
 
 
